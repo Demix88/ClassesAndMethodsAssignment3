3ef7def4a185b813b9b091a3e65579922f741aa3
Demix88 <skrasnauskis@gmail.com>
2022-03-23 10:52:40 +0000

    Add project files.

---
+++ ClassesAndMethodsAssignment3/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment3
+{
+   public class Math
+    {
+        public static void MyMaths(int a = 7, int b = 25)
+        {
+       
+            int z = a * 64;
+            Console.WriteLine("Result of a*64,where \"a\" is "+a+" : "+z+"\nsecond integer is: "+b);
+
+        }
+       
+    }
+ 
+}
---
+++ ClassesAndMethodsAssignment3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment3
+{
+    class Program
+    {
+public static void Main()
+        {
+           Math.MyMaths();
+            Math.MyMaths(182,3);
+            Console.ReadLine();
+
+
+        }
+    }
+}
